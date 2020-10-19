c6c17d79d7eef19c1f787127e9addad38221ecb3
Sebastian Stigler <sebastian.stigler@web.de>
2020-10-19 11:22:50 +0200

    Game class added.

--- game/src/Main.java
+++ game/src/Main.java
@@ -1,6 +1,8 @@
+import game.Game;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello Wordl");
+        new Game().run();
     }
 }
 
---
+++ game/src/game/Game.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Game {
+        public void run(){
+            System.out.println("Game running");
+        }
+}
